8d90978289ba31988f39fb341731cf30ee09695a
Benjamin Miramontes <benjamin@miramontes.dev>
2020-11-25 22:41:13 +0000

    Added consumable item sheet

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -87,91 +87,105 @@ export class ActiveAbilitySheet extends DndItemSheet {
 
 
 export class PassiveAbilitySheet extends DndItemSheet {
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["dnd", "sheet", "item"],
-      template: "systems/5e-lite/templates/passive-ability-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "tab_one"}],
-    });
-  }
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["dnd", "sheet", "item"],
+            template: "systems/5e-lite/templates/passive-ability-sheet.html",
+            width: 520,
+            height: 480,
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description_tab"}],
+        });
+    }
 
-  /** @override */
+    /** @override */
 	activateListeners(html) {
-    super.activateListeners(html);
+        super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-  }
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+    }
 }
 
 
 export class ConsumableSheet extends DndItemSheet {
 
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["dnd", "sheet", "item"],
-      template: "systems/5e-lite/templates/consumable-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "tab_one"}],
-    });
-  }
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["dnd", "sheet", "item"],
+            template: "systems/5e-lite/templates/consumable-sheet.html",
+            width: 520,
+            height: 480,
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description_tab"}],
+        });
+    }
 
-  /** @override */
+    /** @override */
 	activateListeners(html) {
-    super.activateListeners(html);
+        super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-  }
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+
+        html.find(".template .add-button").click(async ev => {
+            ev.preventDefault();
+            this.storeValue("data.template.$id.label", "Label");
+            await this._onSubmit(ev);
+        });
+
+        html.find(".template .delete-button").click(async ev => {
+            ev.preventDefault();
+            const row = ev.target.closest(".row");
+            this.deleteValue("data.template", row.dataset.key);
+            row.parentElement.removeChild(row);
+            await this._onSubmit(ev);
+        });
+    }
 }
 
 
 export class WeaponSheet extends DndItemSheet {
 
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["dnd", "sheet", "item"],
-      template: "systems/5e-lite/templates/weapon-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "tab_one"}],
-    });
-  }
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["dnd", "sheet", "item"],
+            template: "systems/5e-lite/templates/weapon-sheet.html",
+            width: 520,
+            height: 480,
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description_tab"}],
+        });
+    }
 
-  /** @override */
+    /** @override */
 	activateListeners(html) {
-    super.activateListeners(html);
+        super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-  }
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+    }
 }
 
 
 export class MiscSheet extends DndItemSheet {
 
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["dnd", "sheet", "item"],
-      template: "systems/5e-lite/templates/misc-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "tab_one"}],
-    });
-  }
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["dnd", "sheet", "item"],
+            template: "systems/5e-lite/templates/misc-sheet.html",
+            width: 520,
+            height: 480,
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description_tab"}],
+        });
+    }
 
-  /** @override */
+    /** @override */
 	activateListeners(html) {
-    super.activateListeners(html);
+        super.activateListeners(html);
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-  }
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+    }
 }
